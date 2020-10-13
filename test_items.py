cfb374afbc6091dced9199138dff2e3cce385df5
EugeneForStepik <71408811+EugeneForStepik@users.noreply.github.com>
2020-10-13 15:27:19 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -3,4 +3,4 @@ import time
 
 def test_to_find_button_add_to_basket(browser):
     #time.sleep(10)
-    assert browser.find_element_by_class_name( 'btn-add-to-basket11' )
+    assert browser.find_element_by_class_name( 'btn-add-to-basket' )
